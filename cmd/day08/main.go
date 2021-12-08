bae27f7b2c78f5f545407071007dac35565944cd
Steffen Schaffert <dev@tsschaffert.de>
2021-12-08 21:02:29 +0100

    day08: add solution for part 2

--- cmd/day08/main.go
+++ cmd/day08/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"os"
 	"strings"
 )
@@ -17,14 +18,41 @@ func main() {
 
 	uniqueSignals := countUniqueSignals(readings)
 
+	sum := 0
+	analysedReadings := analyseMappings(readings)
+	for _, reading := range analysedReadings {
+		number := convertToNumber(reading)
+		sum += number
+		fmt.Println(number)
+	}
+
 	fmt.Printf("Number of signals with unique of segments: %d\n", uniqueSignals)
+	fmt.Printf("Sum of all numbers in output: %d\n", sum)
 }
 
 type Signal string
 
+func (s Signal) equals(other Signal) bool {
+	if len(s) != len(other) {
+		return false
+	}
+	return s.countOverlap(other) == len(s)
+}
+
+func (s Signal) countOverlap(other Signal) int {
+	overlap := 0
+	for _, char := range s {
+		if strings.ContainsRune(string(other), char) {
+			overlap++
+		}
+	}
+	return overlap
+}
+
 type Reading struct {
 	signalPatterns []Signal
 	output         []Signal
+	mapping        map[Signal]int
 }
 
 func countUniqueSignals(readings []Reading) int {
@@ -38,6 +66,122 @@ func countUniqueSignals(readings []Reading) int {
 		}
 	}
 	return count
+}
+
+func analyseMappings(readings []Reading) []Reading {
+	var mappedReadings []Reading
+
+	for _, reading := range readings {
+		reverseMapping := make(map[int]Signal)
+		mapping := make(map[Signal]int)
+
+		mapTo := 1
+		signal := findMatch(reading.signalPatterns, reverseMapping, func(signal Signal, m map[int]Signal) bool {
+			return len(signal) == 2
+		})
+		mapping[signal] = mapTo
+		reverseMapping[mapTo] = signal
+
+		mapTo = 7
+		signal = findMatch(reading.signalPatterns, reverseMapping, func(signal Signal, m map[int]Signal) bool {
+			return len(signal) == 3
+		})
+		mapping[signal] = mapTo
+		reverseMapping[mapTo] = signal
+
+		mapTo = 4
+		signal = findMatch(reading.signalPatterns, reverseMapping, func(signal Signal, m map[int]Signal) bool {
+			return len(signal) == 4
+		})
+		mapping[signal] = mapTo
+		reverseMapping[mapTo] = signal
+
+		mapTo = 8
+		signal = findMatch(reading.signalPatterns, reverseMapping, func(signal Signal, m map[int]Signal) bool {
+			return len(signal) == 7
+		})
+		mapping[signal] = mapTo
+		reverseMapping[mapTo] = signal
+
+		mapTo = 3
+		signal = findMatch(reading.signalPatterns, reverseMapping, func(signal Signal, m map[int]Signal) bool {
+			return len(signal) == 5 && signal.countOverlap(m[7]) == len(m[7])
+		})
+		mapping[signal] = mapTo
+		reverseMapping[mapTo] = signal
+
+		mapTo = 5
+		signal = findMatch(reading.signalPatterns, reverseMapping, func(signal Signal, m map[int]Signal) bool {
+			return len(signal) == 5 && !signal.equals(m[3]) && signal.countOverlap(m[4]) == 3
+		})
+		mapping[signal] = mapTo
+		reverseMapping[mapTo] = signal
+
+		mapTo = 2
+		signal = findMatch(reading.signalPatterns, reverseMapping, func(signal Signal, m map[int]Signal) bool {
+			return len(signal) == 5 && !signal.equals(m[3]) && !signal.equals(m[5])
+		})
+		mapping[signal] = mapTo
+		reverseMapping[mapTo] = signal
+
+		mapTo = 6
+		signal = findMatch(reading.signalPatterns, reverseMapping, func(signal Signal, m map[int]Signal) bool {
+			return len(signal) == 6 && signal.countOverlap(m[7]) == 2
+		})
+		mapping[signal] = mapTo
+		reverseMapping[mapTo] = signal
+
+		mapTo = 9
+		signal = findMatch(reading.signalPatterns, reverseMapping, func(signal Signal, m map[int]Signal) bool {
+			return len(signal) == 6 && !signal.equals(m[6]) && signal.countOverlap(m[4]) == len(m[4])
+		})
+		mapping[signal] = mapTo
+		reverseMapping[mapTo] = signal
+
+		mapTo = 0
+		signal = findMatch(reading.signalPatterns, reverseMapping, func(signal Signal, m map[int]Signal) bool {
+			return len(signal) == 6 && !signal.equals(m[6]) && !signal.equals(m[9])
+		})
+		mapping[signal] = mapTo
+		reverseMapping[mapTo] = signal
+
+		mappedReadings = append(mappedReadings, Reading{
+			signalPatterns: reading.signalPatterns,
+			output:         reading.output,
+			mapping:        mapping,
+		})
+	}
+
+	return mappedReadings
+}
+
+func convertToNumber(reading Reading) int {
+	result := 0
+	for index, output := range reading.output {
+		result += lookupInMap(output, reading.mapping) * int(math.Pow10(3-index))
+	}
+	return result
+}
+
+func lookupInMap(output Signal, mapping map[Signal]int) int {
+	for signal, value := range mapping {
+		if signal.equals(output) {
+			return value
+		}
+	}
+	log.Fatalf("no match found for %s in %v", output, mapping)
+	return -1
+}
+
+func findMatch(signalPatterns []Signal, reverseMapping map[int]Signal, matches func(Signal, map[int]Signal) bool) Signal {
+	for _, signal := range signalPatterns {
+		if matches(signal, reverseMapping) {
+			return signal
+		}
+	}
+	// TODO return error
+	log.Fatalf("no match found")
+	return ""
 }
 
 func readSignals(input io.Reader) ([]Reading, error) {
--- cmd/day08/main_test.go
+++ cmd/day08/main_test.go
@@ -75,3 +75,313 @@ func Test_countUniqueSignals(t *testing.T) {
 		})
 	}
 }
+
+func Test_analyseMappings(t *testing.T) {
+	type args struct {
+		readings []Reading
+	}
+	tests := []struct {
+		name string
+		args args
+		want []Reading
+	}{
+		{
+			name: "website example",
+			args: args{
+				[]Reading{
+					{
+						signalPatterns: []Signal{"acedgfb", "cdfbe", "gcdfa", "fbcad", "dab", "cefabd", "cdfgeb", "eafb", "cagedb", "ab"},
+						output:         []Signal{"cdfeb", "fcadb", "cdfeb", "cdbaf"},
+						mapping:        nil,
+					},
+				},
+			},
+			want: []Reading{
+				{
+					signalPatterns: []Signal{"acedgfb", "cdfbe", "gcdfa", "fbcad", "dab", "cefabd", "cdfgeb", "eafb", "cagedb", "ab"},
+					output:         []Signal{"cdfeb", "fcadb", "cdfeb", "cdbaf"},
+					mapping: map[Signal]int{
+						"acedgfb": 8,
+						"cdfbe":   5,
+						"gcdfa":   2,
+						"fbcad":   3,
+						"dab":     7,
+						"cefabd":  9,
+						"cdfgeb":  6,
+						"eafb":    4,
+						"cagedb":  0,
+						"ab":      1,
+					},
+				},
+			},
+		},
+		{
+			name: "3 and 5 overlap example",
+			args: args{
+				[]Reading{
+					{
+						signalPatterns: []Signal{"aceg", "gdaef", "fcbegda", "dcefab", "afcedg", "abgdfc", "cadef", "agf", "degbf", "ag"},
+						output:         []Signal{"bdgfe", "bacfgd", "cadfe", "fcgeabd"},
+						mapping:        nil,
+					},
+				},
+			},
+			want: []Reading{
+				{
+					signalPatterns: []Signal{"aceg", "gdaef", "fcbegda", "dcefab", "afcedg", "abgdfc", "cadef", "agf", "degbf", "ag"},
+					output:         []Signal{"bdgfe", "bacfgd", "cadfe", "fcgeabd"},
+					mapping: map[Signal]int{
+						"fcbegda": 8,
+						"degbf":   2,
+						"cadef":   5,
+						"gdaef":   3,
+						"agf":     7,
+						"afcedg":  9,
+						"dcefab":  6,
+						"aceg":    4,
+						"abgdfc":  0,
+						"ag":      1,
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := analyseMappings(tt.args.readings); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("analyseMappings() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSignal_equals(t *testing.T) {
+	type args struct {
+		other Signal
+	}
+	tests := []struct {
+		name string
+		s    Signal
+		args args
+		want bool
+	}{
+		{
+			name: "simple match",
+			s:    "a",
+			args: args{other: "a"},
+			want: true,
+		},
+		{
+			name: "simple non-match",
+			s:    "a",
+			args: args{other: "b"},
+			want: false,
+		},
+		{
+			name: "transposition match",
+			s:    "ab",
+			args: args{other: "ba"},
+			want: true,
+		},
+		{
+			name: "transposition non-match",
+			s:    "ab",
+			args: args{other: "ca"},
+			want: false,
+		},
+		{
+			name: "different length",
+			s:    "ab",
+			args: args{other: "abc"},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.s.equals(tt.args.other); got != tt.want {
+				t.Errorf("equals() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSignal_countOverlap(t *testing.T) {
+	type args struct {
+		other Signal
+	}
+	tests := []struct {
+		name string
+		s    Signal
+		args args
+		want int
+	}{
+		{
+			name: "simple test",
+			s:    "a",
+			args: args{other: "a"},
+			want: 1,
+		},
+		{
+			name: "simple test",
+			s:    "a",
+			args: args{other: "b"},
+			want: 0,
+		},
+		{
+			name: "transposition match",
+			s:    "ab",
+			args: args{other: "ba"},
+			want: 2,
+		},
+		{
+			name: "transposition non-match",
+			s:    "ab",
+			args: args{other: "ca"},
+			want: 1,
+		},
+		{
+			name: "different length",
+			s:    "ab",
+			args: args{other: "abc"},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.s.countOverlap(tt.args.other); got != tt.want {
+				t.Errorf("countOverlap() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_findMatch(t *testing.T) {
+	type args struct {
+		signalPatterns []Signal
+		reverseMapping map[int]Signal
+		matches        func(Signal, map[int]Signal) bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want Signal
+	}{
+		{
+			name: "simple test",
+			args: args{
+				signalPatterns: []Signal{"a"},
+				reverseMapping: nil,
+				matches: func(signal Signal, m map[int]Signal) bool {
+					return signal == "a"
+				},
+			},
+			want: "a",
+		},
+		{
+			name: "website example",
+			args: args{
+				signalPatterns: []Signal{"acedgfb", "cdfbe", "gcdfa", "fbcad", "dab", "cefabd", "cdfgeb", "eafb", "cagedb", "ab"},
+				reverseMapping: nil,
+				matches: func(signal Signal, m map[int]Signal) bool {
+					return len(signal) == 2
+				},
+			},
+			want: "ab",
+		},
+		{
+			name: "website example with existing mapping",
+			args: args{
+				signalPatterns: []Signal{"acedgfb", "cdfbe", "gcdfa", "fbcad", "dab", "cefabd", "cdfgeb", "eafb", "cagedb", "ab"},
+				reverseMapping: map[int]Signal{7: "dab"},
+				matches: func(signal Signal, m map[int]Signal) bool {
+					return len(signal) == 5 && signal.countOverlap(m[7]) == len(m[7])
+				},
+			},
+			want: "fbcad",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findMatch(tt.args.signalPatterns, tt.args.reverseMapping, tt.args.matches); got != tt.want {
+				t.Errorf("findMatch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_convertToNumber(t *testing.T) {
+	type args struct {
+		reading Reading
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "website example",
+			args: args{
+				reading: Reading{
+					signalPatterns: []Signal{"acedgfb", "cdfbe", "gcdfa", "fbcad", "dab", "cefabd", "cdfgeb", "eafb", "cagedb", "ab"},
+					output:         []Signal{"cdfeb", "fcadb", "cdfeb", "cdbaf"},
+					mapping: map[Signal]int{
+						"acedgfb": 8,
+						"cdfbe":   5,
+						"gcdfa":   2,
+						"fbcad":   3,
+						"dab":     7,
+						"cefabd":  9,
+						"cdfgeb":  6,
+						"eafb":    4,
+						"cagedb":  0,
+						"ab":      1,
+					},
+				},
+			},
+			want: 5353,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := convertToNumber(tt.args.reading); got != tt.want {
+				t.Errorf("convertToNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_lookupInMap(t *testing.T) {
+	type args struct {
+		output  Signal
+		mapping map[Signal]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "simple test",
+			args: args{
+				output:  "a",
+				mapping: map[Signal]int{"a": 1},
+			},
+			want: 1,
+		},
+		{
+			name: "another test",
+			args: args{
+				output:  "ab",
+				mapping: map[Signal]int{"a": 1, "ba": 2},
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lookupInMap(tt.args.output, tt.args.mapping); got != tt.want {
+				t.Errorf("lookupInMap() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
